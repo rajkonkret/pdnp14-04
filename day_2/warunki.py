58d36bfa375dea9d0b96d18bf2f1dc0c29261dd0
rajkonkret <rajkonkret660@gmail.com>
2025-04-15 15:29:04 +0200

    [2 14] update warunki.py

--- day_2/warunki.py
+++ day_2/warunki.py
@@ -45,3 +45,47 @@ if n > 3:
 # operator morsa, walrus
 if (n := len(a)) > 3:
     print(f'Długosc wynosi {n}, więcej niż 3')
+
+# kolejnośc warunków ma znaczenie
+# # pierwszy true, wyjście z drzewka
+# podatek = 0
+# zarobki = int(input("Podaj zarobki"))
+# if zarobki < 10000:
+#     podatek = 0
+# elif zarobki < 39999:
+#     podatek = 0.2
+# elif zarobki < 100_000:
+#     podatek = 0.4
+# else:
+#     podatek = 0.9
+#
+# print(f"Podatek wynosi {podatek * zarobki} pln.")
+# # podatek 0.2 dla przedziału 10000 do 39999
+
+suma_zam = 150
+if suma_zam > 100:
+    rabat = 25
+else:
+    rabat = 0
+
+print(f'Rabat wynosi {rabat}')
+
+rabat = 25 if suma_zam > 100 else 0  # operator warunkowy
+print(f'Rabat wynosi {rabat}')
+
+# napisac program test z...
+#
+punkty = 0
+odp = input("Podaj datę chrztu Polski")  # str
+if odp.strip().casefold() == "966":
+    print("dobrze")
+    punkty += 1  # punkty = punkty + 1
+else:
+    print("Idź sie pouczyć")
+
+print("Zdobyte punkty:", punkty)
+# spam += 1    spam = spam + 1
+# spam -= 1    spam = spam - 1
+# spam *= 1    spam = spam * 1
+# spam /= 1    spam = spam / 1
+# spam %= 1    spam = spam % 1
