72a2b6e09e272bb98fd632d54ff1e0d5037028f8
rajkonkret <rajkonkret660@gmail.com>
2025-04-16 14:37:51 +0200

    [3 12] add pliki_zad1.py

---
+++ day_3/pliki_zad1.py
@@ -0,0 +1,49 @@
+# działania z plikami
+# filehandler - rura do pliku
+# context manager pomaga w pracy z plikami
+# with - context manager w python
+
+# ========= ===============================================================
+#     Character Meaning
+#     --------- ---------------------------------------------------------------
+#     'r'       open for reading (default)
+#     'w'       open for writing, truncating the file first
+#     'x'       create a new file and open it for writing
+#     'a'       open for writing, appending to the end of the file if it exists
+#     'b'       binary mode
+#     't'       text mode (default)
+#     '+'       open a disk file for updating (reading and writing)
+#     ========= ===============================================================
+with open('test.log', "w", encoding='utf-8') as fh:
+    fh.write("Powitanie\n")
+    fh.write("Kolejne\n")
+    fh.write("Jeszcze jedno\n")
+
+# plik istnieje
+# FileExistsError: [Errno 17] File exists: 'test.log'
+# with open('test.log', "x") as f:
+#     f.write("Powitanie")
+
+# "w" kasuje plik i tworzy nowy
+with open('test.log', "w", encoding='utf-8') as fh:
+    fh.write("nowe\n")
+    fh.write("nowe\n")
+    fh.write("nowe\n")
+
+# dodaje do istniejacego pliku
+with open('test.log', "a", encoding='utf-8') as fh:
+    fh.write("Powitanie\n")
+    fh.write("Kolejne\n")
+    fh.write("Jeszcze jedno\n")
+    fh.write("dośdane\n")
+
+with open('test.log', "r", encoding='utf-8') as file:
+    lines = file.read()
+
+print(lines)
+# 3 nowe
+# nowe
+# nowe
+# Powitanie
+# Kolejne
+# Jeszcze jedno
