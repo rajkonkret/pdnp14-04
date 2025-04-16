66f1bb360503598e966102fbc87a9dd8ef93d868
rajkonkret <rajkonkret660@gmail.com>
2025-04-16 11:12:17 +0200

    [3 6] update petle_zad1.py

--- day_3/petle_zad1.py
+++ day_3/petle_zad1.py
@@ -26,3 +26,54 @@ for _ in range(6):
     lista_wyn.append(wyn)
 
 print(lista_wyn)  # [14, 11, 23, 4, 19, 6]
+
+for i in range(10):
+    if i % 2 == 0:
+        print(i, "parzysta")
+# 0 parzysta
+# 2 parzysta
+# 4 parzysta
+# 6 parzysta
+# 8 parzysta
+
+# list comprehension
+lista3 = [j for j in range(10) if j % 2 == 0]
+print(lista3)  # [0, 2, 4, 6, 8]
+
+for c in lista3:
+    if c > 4:
+        print("Większe od 4")
+    else:
+        print("Mniejsze, równe 4")
+# Mniejsze, równe 4
+# Mniejsze, równe 4
+# Mniejsze, równe 4
+# Większe od 4
+# Większe od 4
+
+for i in range(0, 10, 2):  # start, stop, krok, krok co 2
+    print(i)
+# 0
+# 2
+# 4
+# 6
+# 8
+for i in range(-10, 0):
+    print(i)
+
+for i in range(10, 0, -1):  # krok ujemny
+    print(i)
+
+for c in lista3:
+    if c == 2:
+        c += 1  # c = c + 1
+        print(c)  # gdy warunek spełniony
+    print('Przy kazdym przejsciu pętli')
+# Przy kazdym przejsciu pętli
+# 3
+# Przy kazdym przejsciu pętli
+# Przy kazdym przejsciu pętli
+# Przy kazdym przejsciu pętli
+# Przy kazdym przejsciu pętli
+
+imiona = ["Radek", 'Tomek', "Zenek", "Ania"]
