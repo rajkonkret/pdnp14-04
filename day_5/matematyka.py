64a0b0a75b1b19f3e7ebd6a5771d3c9636e8cdf5
rajkonkret <rajkonkret660@gmail.com>
2025-04-18 11:14:07 +0200

    [5 6] add matematyka

---
+++ day_5/matematyka.py
@@ -0,0 +1,10 @@
+import math
+
+print(math.pi)  # 3.141592653589793
+print(math.sqrt(25))  # 5.0
+
+angle_degrees = 30
+angle_radians = math.radians(angle_degrees)
+sin_values = math.sin(angle_radians)
+print(sin_values)
+# 0.49999999999999994
