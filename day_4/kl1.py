28f92020a3b6be038673e45aa6a5cf15f91ede5e
rajkonkret <rajkonkret660@gmail.com>
2025-04-18 08:54:39 +0200

    [5 1] update kl1

--- day_4/kl1.py
+++ day_4/kl1.py
@@ -14,6 +14,16 @@ class Human:
     Klasa Human w Pythonie
     """
 
+    imie = ""
+    wiek = None
+    plec = "k"
+
+    def powitanie(self):
+        print(f"Nazywam się {self.imie}")
+
+    def wypisz_wiek(self):
+        print(f"Mam {self.wiek} ")
+
 
 cz1 = Human()
 print(cz1)
@@ -23,3 +33,18 @@ print(print.__doc__)
 # cd.\day_4\
 # pydoc -b
 # pydoc -w kl1
+print(cz1.imie)
+print(cz1.wiek)
+print(cz1.plec)
+# None
+# k
+cz1.wiek = 45
+cz1.imie = "Anna"
+print(cz1.imie)
+print(cz1.wiek)
+print(cz1.plec)
+# Anna
+# 45
+# k
+cz1.powitanie()
+cz1.wypisz_wiek()
